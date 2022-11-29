3fb68ab03eb2ddae4974b1bf9dfe0efb329db7af
Figueiras <antoniorfigueiras@gmail.com>
2022-11-29 15:43:23 +0000

    teste5

---
+++ gymmethod/backend/views/layouts/sidebar.php
@@ -0,0 +1,47 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">GymMethodas</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="<?=$assetDir?>/img/user1-128x128.jpg" class="fa-solid fa-user img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block"><?= Yii::$app->user->identity->username; ?></a>
+            </div>
+        </div>
+
+
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <?php
+            echo \hail812\adminlte\widgets\Menu::widget([
+                'items' => [
+                    ['label' => 'Produtos', 'icon' => 'store', 'url' => ['/produto'],],
+                    ['label' => 'Funcionarios', 'icon' => 'user', 'url' => ['/funcionario'],],
+                   // ['label' => 'Perfil', 'icon' => 'user', 'url' => ['/perfil'],],
+                    ['label' => 'Yii2 PROVIDED', 'header' => true],
+                    //['label' => 'Logout', 'url' => ['site/logout'], 'icon' => 'sign-out-alt', 'visible' => Yii::$app->user->can("loginBO")],
+
+
+
+
+
+                    /* YII & DEBUG*/
+                    ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
+                    ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
+                ],
+            ]);
+            ?>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
