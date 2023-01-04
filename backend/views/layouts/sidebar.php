574274fb37cea7a033a1e47bd70700671a720d8f
Figueiras <antoniorfigueiras@gmail.com>
2023-01-04 16:54:30 +0000

    Fix

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -5,8 +5,8 @@ use hail812\adminlte\widgets\Menu;
 ?>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?=\yii\helpers\Url::home()?>" class="brand-link">
-        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="GymMethod Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="<?=\yii\helpers\Url::home()?>" class="brand-link text-decoration-none">
+        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="GymMethod Logo" class="brand-image img-circle elevation-3 " style="opacity: .8;">
         <span class="brand-text font-weight-light">GymMethod</span>
     </a>
 
@@ -15,7 +15,7 @@ use hail812\adminlte\widgets\Menu;
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="info">
-                <a class="d-block">Bem Vindo: <?= Yii::$app->user->identity->username; ?></a>
+                <a class="d-block text-decoration-none">Bem Vindo: <?= Yii::$app->user->identity->username; ?></a>
             </div>
         </div>
 
